b42fe17b54856dbc075289a0f4af2cf1aa550a17
Dane Marguglio <marguglio.dane@gmail.com>
2023-12-03 14:16:32 -0500

    Add types to puzzle 2

--- 2023/puzzles/puzzle_2.py
+++ 2023/puzzles/puzzle_2.py
@@ -1,69 +1,57 @@
 from util import load_input_file
+from typing import List, Dict, Tuple
 
 
-# e.g. Game 1: 4 red, 8 green; 8 green, 6 red; 13 red, 8 green; 2 blue, 4 red, 4 green
-def process_line(line: str):
+def process_line(line: str) -> Tuple[int, List[Dict[str, int]]]:
+    """ Process a single line of input to extract game number and cube counts. """
+    # Splitting the line to extract game number and cube strings
     game_number = int(line.split(":")[0].split(" ")[1])
     game_strs = line.split(":")[1].strip().split(';')
-    games = [] # [{'red': 2, 'blue': 11, 'green': 2}, ... ]
+    games = [] 
     for game in game_strs:
         cube_strs = [i.strip() for i in game.split(",")]
         cube_pairs = [i.split(' ') for i in cube_strs]
-        cubes = {i[1]:int(i[0]) for i in cube_pairs}
+        cubes = {i[1]: int(i[0]) for i in cube_pairs}
         games.append(cubes)
+        
     return game_number, games
-    
-#  only 12 red cubes, 13 green cubes, and 14 blue cubes   
-def game_is_possible(game, max_cubes = {'red': 12, 'green': 13, 'blue': 14 }):
+
+def game_is_possible(game: Dict[str, int], max_cubes: Dict[str, int] = {'red': 12, 'green': 13, 'blue': 14}) -> bool:
+    """ Check if the game configuration is possible within the max cube limits. """
     for color in max_cubes:
-        if color not in game:
-            continue
-        if game[color] > max_cubes[color]:
+        if color in game and game[color] > max_cubes[color]:
             return False
     return True
 
-# Get min cubes for a game
-def get_min_cubes(games):
+def get_min_cubes(games: List[Dict[str, int]]) -> Dict[str, int]:
+    """ Determine the minimum number of cubes required for each color. """
     min_cubes = {'red': 0, 'green': 0, 'blue': 0}
     for game in games:
         for color in game:
-            if color not in min_cubes:
-                min_cubes[color] = game[color]
-            else:
-                min_cubes[color] = max(min_cubes[color], game[color])
+            min_cubes[color] = max(min_cubes.get(color, 0), game[color])
     return min_cubes
-    
 
-input_data = load_input_file(2)
-
-def part_one(input_data: list[str] = input_data) -> int:
+def part_one(input_data: List[str]) -> int:
     total = 0
     for line in input_data:
         game_number, games = process_line(line)
-        impossible = False
-        
-        for game in games:
-            if not game_is_possible(game):
-                impossible = True
-                break
-        
-        if not impossible:
+        if all(game_is_possible(game) for game in games):
             total += game_number
     return total
 
-
-def part_two(input_data: list[str] = input_data) -> int:
+def part_two(input_data: List[str]) -> int:
     total = 0
     for line in input_data:
-        game_number, games = process_line(line)
+        _, games = process_line(line)
         cubes = get_min_cubes(games)
         product = 1
         for color in cubes:
             product *= cubes[color]
         total += product
     return total
-    
 
 
-print(f'Part one: {part_one()}')
-print(f'Part two: {part_two()}')
+if __name__ == '__main__':
+    input_data = load_input_file(2)
+    print(f'Part one: {part_one(input_data)}')
+    print(f'Part two: {part_two(input_data)}')
