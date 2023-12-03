09f27d99a9f3448a7e26fa9bf7d9653b934c2318
Dane Marguglio <marguglio.dane@gmail.com>
2023-12-03 14:18:40 -0500

    Clean up puzzle 1

--- 2023/puzzles/puzzle_1.py
+++ 2023/puzzles/puzzle_1.py
@@ -1,7 +1,6 @@
 import re
 from typing import List
-
-
+from util import load_input_file
 
 DIGITS = {
     'one': 1,
@@ -16,67 +15,53 @@ DIGITS = {
 }
 
 
-def load_input_file(puzzle_number) -> list[str]:
-    """ Load the input file for the given puzzle number """
-    file_name = f'../input/input_{puzzle_number}.txt'
-    try:
-        with open(file_name, 'r') as f:
-            return [line.strip() for line in f.readlines()]
-    except FileNotFoundError:
-        print(f'File not found: {file_name}')
-        return []
-
-
 def get_calibration(line: str) -> int:
-    """ Calibration for a line is defined by combining first and last digit into a single number """
+    """Determine calibration by combining the first and last digit found in the string."""
     first, last = None, None
     for character in line:
         if character.isdigit():
             if first is None:
                 first = int(character)
             last = int(character)
-    if last == None:
+    if last is None:
         raise Exception(f"Could not find any digits in {line}")
-    return 10*first + last
-
+    return 10 * first + last
 
 def get_calibration_2(line: str) -> int:
-    """ Part 2: Same thing but with string representations too """
+    """Extend calibration to include word representations of digits."""
     find_all = lambda in_string, to_find: [k.start() for k in re.finditer(to_find, in_string)]
     
     ints: List[int] = []
     
-    # Find index of all digits
+    # Find index and value of all digits
     for index, character in enumerate(line):
         if character.isdigit():
             ints.append((index, int(character)))
     
-    # Find index of all words representing digits, slow but who cares
+    # Find index and value of all word representations of digits
     for digit in DIGITS:
         if digit in line:
             for index in find_all(line, digit):
                 ints.append((index, DIGITS[digit]))
                 
-    # Sort tuples by index, get first and last
+    # Sort tuples by index and combine first and last digit found
     ints.sort(key=lambda x: x[0])
-    return 10*ints[0][1] + ints[-1][1]
+    return 10 * ints[0][1] + ints[-1][1]
 
-""" Part 1: Find the sum of all calibrations in the input file """
 def part_1(lines: List[str]) -> int:
+    """Calculate the sum of calibrations for all lines."""
     return sum([get_calibration(line) for line in lines])
 
-
-""" Part 2: Same thing but with string numbers too """
 def part_2(lines: List[str]) -> int:
+    """Calculate the sum of calibrations for all lines, including string numbers."""
     return sum([get_calibration_2(line) for line in lines])
 
-
 if __name__ == "__main__":
-    assert(get_calibration('1any_thing_here_1_2_9') == 19)
-    assert(get_calibration('19one') == 19)
-    assert(get_calibration('1') == 11)
-    assert(get_calibration_2('19one') == 11)
-    assert(get_calibration_2('eightwo') == 82)
+    assert get_calibration('1any_thing_here_1_2_9') == 19
+    assert get_calibration('19one') == 19
+    assert get_calibration('1') == 11
+    assert get_calibration_2('19one') == 11
+    assert get_calibration_2('eightwo') == 82
 
     lines = load_input_file(1)
     print(f"Part 1: {part_1(lines)}")
